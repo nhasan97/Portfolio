2294d7cb4860a550b625aedae541b739e30223e3
nhasan97 <nh6069325@gmail.com>
2024-11-20 16:10:15 +0600

    created TUserProfileUpdate type in profile.interface.ts, created getMyProfile and updateMyProfile controller functions in profile.controller.ts, created getMyProfile and updateMyProfile service functions in profile,service.ts and created a patch and a get route in profile.route.ts

---
+++ Portfolio-Server/src/app/modules/Profile/profile.route.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../User/user.constant';
+import { ProfileController } from './profile.controller';
+import { multerUpload } from '../../config/multer.config';
+import { parseBody } from '../../middlewares/bodyParser';
+
+const router = express.Router();
+
+router.get('/', auth(USER_ROLE.ADMIN), ProfileController.getMyProfile);
+
+router.patch(
+  '/',
+  auth(USER_ROLE.ADMIN),
+  multerUpload.single('profilePhoto'),
+  parseBody,
+  ProfileController.updateMyProfile
+);
+
+export const ProfileRoutes = router;
